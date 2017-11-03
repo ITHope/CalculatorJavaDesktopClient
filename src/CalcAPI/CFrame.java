61f3838ba5ef7ac90fa2ce9bbe92f0fa3ea9b200
Nadiia Berdnyk <nadya.berdnik@mail.ru>
2017-11-03 23:31:46 +0200

    Add Project Files

---
+++ src/CalcAPI/CFrame.java
@@ -0,0 +1,20 @@
+package CalcAPI;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+public class CFrame extends JFrame 
+{
+	public CFrame() 
+	{
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Calc");
+		setBounds(300,100,250,250);
+		JTextField inp=new JTextField();
+		inp.setEnabled(true);
+		add(inp);
+		CPanel cp=new CPanel();
+		cp.setBackground(Color.white);
+		add(cp);
+		setVisible(true);
+	}
+}
---
+++ src/CalcAPI/CFrameButtons.java
@@ -0,0 +1,9 @@
+package CalcAPI;
+
+public class CFrameButtons
+{
+	public static void main(String[] args)
+	{
+		new CFrame();
+	}
+}
